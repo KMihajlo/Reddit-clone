f24a9157bfe81c852234d9c6aa6e5bbe50da82fb
KMihajlo <mihajlo.kragujevski@yahoo.com>
2021-08-27 02:40:36 +0200

    syntax fixes..

--- src/scripts/docker-grafana/docker-run-grafana.sh
+++ src/scripts/docker-grafana/docker-run-grafana.sh
@@ -3,6 +3,4 @@
 # "--name" choose a name for your container
 # "port:port" choose a port to place grafana container
 
-!bin
-
 docker run -d --name=grafana -p 3000:3000 grafana/grafana
--- src/scripts/docker-prometheus/docker-start-prom.sh
+++ src/scripts/docker-prometheus/docker-start-prom.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #used every other time to start the container.
 docker start nervous_leakey
