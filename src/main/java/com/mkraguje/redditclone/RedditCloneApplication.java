d3bb8e7902783992e437937d4e31219ea0102b26
KMihajlo <mihajlo.kragujevski@yahoo.com>
2021-09-26 02:14:40 +0200

    Implemented @Transactional method

--- src/main/java/com/mkraguje/redditclone/RedditCloneApplication.java
+++ src/main/java/com/mkraguje/redditclone/RedditCloneApplication.java
@@ -4,11 +4,13 @@ import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class RedditCloneApplication {
 
 	public static void main(String[] args) {
